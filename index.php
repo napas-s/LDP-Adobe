2fcb3ee7f0fd64565374dd80ac292245e2c09dde
napassorn <yui.napassorn.s@gmail.com>
2021-06-24 12:44:40 +0700

    delete livechat

--- index.php
+++ index.php
@@ -478,21 +478,7 @@
 		});
 	</script>
 	
-	<!-- Start of LiveChat (www.livechatinc.com) code -->
-	<script type="text/javascript">
-	window.__lc = window.__lc || {};
-	window.__lc.license = 10843677;
-	(function() {
-		var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
-		lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
-	})();
-	</script>
-	<noscript>
-	<a href="https://www.livechatinc.com/chat-with/10843677/" rel="nofollow">Chat with us</a>,
-	powered by <a href="https://www.livechatinc.com/?welcome" rel="noopener nofollow" target="_blank">LiveChat</a>
-	</noscript>
-	<!-- End of LiveChat code -->
+	
 
 
 </body>
